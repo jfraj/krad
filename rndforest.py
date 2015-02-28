895e02624e61f9e2c268f6ebb348a45508a233e4
jfraj <rajotte.jeanfrancois@gmail.com>
2015-02-28 18:55:52 -0500

    Added optimizations and verbose features

--- rndforest.py
+++ rndforest.py
@@ -14,9 +14,18 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.learning_curve import validation_curve
 from sklearn.learning_curve import learning_curve
 
+## Ressources
+## Not used yet, but it might be useful to select n-1 core
+## to still have some power for other tasks
+## number of available cpu = multiprocessing.cpu_count()
+import multiprocessing
+import gc
+
 ## krad import
 import clean, solution
 from score import kaggle_metric, heaviside, poisson_cumul
+
+
 
 
 feature_dic = {\
@@ -43,69 +52,88 @@ class RandomForestModel(object):
         print 'Creating training data frame with shape'
         print self.df_train.shape
 
-    def prepare_data(self, df):
+    def prepare_data(self, df, verbose = False, var2prep = 'all'):
         """
         prepare self.df_train for fitting
+        var2prep is a list of variables that will be needed.
+        This will save time by cleaning only the needed variables
         """
+        if verbose:
+            print 'Getting radar length'
         df['RadarCounts'] = df['TimeToEnd'].apply(clean.getRadarLength)
 
+        #######
         ## Adding the mean of variables to fit
 
         ## Reflectivity
-        df['Reflectivity1'] = df[['RadarCounts','Reflectivity']].apply(
-            clean.getIthRadar, axis=1)
-        df['Avg_Reflectivity'],  df['Range_Reflectivity'], df['Nval']=\
-          zip(*df['Reflectivity1'].apply(clean.getListReductions))
+        if var2prep == 'all' or any("Reflectivity" in s for s in var2prep):
+            if verbose:
+                print 'Clean reflectivity'
+            df['Reflectivity1'] = df[['RadarCounts','Reflectivity']].apply(
+                clean.getIthRadar, axis=1)
+            df['Avg_Reflectivity'],  df['Range_Reflectivity'], df['Nval']=\
+              zip(*df['Reflectivity1'].apply(clean.getListReductions))
 
         ## Distance to radar
-        df['DistanceToRadar1'] = df[['RadarCounts','DistanceToRadar']].apply(clean.getIthRadar,
-                                                                             axis=1)
-        df['Avg_DistanceToRadar'],  df['Range_DistanceToRadar'], df['Nval_DistanceToRadar']=\
-          zip(*df['DistanceToRadar1'].apply(clean.getListReductions))
-        ## Remove the Nval_xxx it's already in the Nval column 
-        df.drop('Nval_DistanceToRadar', axis=1, inplace=True)
+        if var2prep == 'all' or any("DistanceToRadar" in s for s in var2prep):
+            if verbose:
+                print 'Clean DistanceToRadar'
+            df['DistanceToRadar1'] = df[['RadarCounts','DistanceToRadar']].apply(clean.getIthRadar, axis=1)
+            df['Avg_DistanceToRadar'],  df['Range_DistanceToRadar'], df['Nval_DistanceToRadar']=\
+              zip(*df['DistanceToRadar1'].apply(clean.getListReductions))
+            ## Remove the Nval_xxx it's already in the Nval column 
+            df.drop('Nval_DistanceToRadar', axis=1, inplace=True)
 
         ## Radar quality index
-        df['RadarQualityIndex1'] = df[['RadarCounts','RadarQualityIndex']].apply(clean.getIthRadar,
-                                                                                 axis=1)
-        df['Avg_RadarQualityIndex'],  df['Range_RadarQualityIndex'], df['Nval_RadarQualityIndex']=\
-          zip(*df['RadarQualityIndex1'].apply(clean.getListReductions))
-        df.drop('Nval_RadarQualityIndex', axis=1, inplace=True)# Already in Nval
-        ## Set Avg_RadarQualityIndex above 1 (could not be computed) to 0.5 i.e. average data
-        ## (any element in the list above 999 will make the average above 1)
-        df.loc[df.Avg_RadarQualityIndex > 1, 'Avg_RadarQualityIndex'] = 0.5
-        ## Set All the < 0 (something wrong with measurement) as 0 i.e. bad data
-        df.loc[df.Avg_RadarQualityIndex < 0, 'Avg_RadarQualityIndex'] = 0.0
+        if var2prep == 'all' or any("RadarQualityIndex" in s for s in var2prep):
+            if verbose:
+                print 'Clean RadarQualityIndex'
+            df['RadarQualityIndex1'] =\
+              df[['RadarCounts','RadarQualityIndex']].apply(clean.getIthRadar,axis=1)
+            df['Avg_RadarQualityIndex'],  df['Range_RadarQualityIndex'], df['Nval_RadarQualityIndex']=\
+              zip(*df['RadarQualityIndex1'].apply(clean.getListReductions))
+            df.drop('Nval_RadarQualityIndex', axis=1, inplace=True)# Already in Nval
+            ## Set Avg_RadarQualityIndex above 1 (could not be computed) to 0.5 i.e. average data
+            ## (any element in the list above 999 will make the average above 1)
+            df.loc[df.Avg_RadarQualityIndex > 1, 'Avg_RadarQualityIndex'] = 0.5
+            ## Set All the < 0 (something wrong with measurement) as 0 i.e. bad data
+            df.loc[df.Avg_RadarQualityIndex < 0, 'Avg_RadarQualityIndex'] = 0.0
 
         ##RR1
-        df['RR11'] = df[['RadarCounts','RR1']].apply(clean.getIthRadar,
-                                                                                 axis=1)
-        df['Avg_RR1'],  df['Range_RR1'], df['Nval_RR1']=\
-          zip(*df['RR11'].apply(clean.getListReductions))
-        df.drop('Nval_RR1', axis=1, inplace=True)# Already in Nval
-        ## Set negative RR1 (could not be computed) to 0.0 i.e. no rain
-        ## (any element in the list with error code (<=-99000) will make the average negative)
-        df.loc[df.Avg_RR1 < 1, 'Avg_RR1'] = 0.0
+        if var2prep == 'all' or any("RR1" in s for s in var2prep):
+            if verbose:
+                print 'Clean RR1'
+            df['RR11'] = df[['RadarCounts','RR1']].apply(clean.getIthRadar, axis=1)
+            df['Avg_RR1'],  df['Range_RR1'], df['Nval_RR1']=\
+              zip(*df['RR11'].apply(clean.getListReductions))
+            df.drop('Nval_RR1', axis=1, inplace=True)# Already in Nval
+            ## Set negative RR1 (could not be computed) to 0.0 i.e. no rain
+            ## (elements in the list with error code (<=-99000) will make the average negative)
+            df.loc[df.Avg_RR1 < 1, 'Avg_RR1'] = 0.0
         
         ##RR2
-        df['RR21'] = df[['RadarCounts','RR2']].apply(clean.getIthRadar,
-                                                                                 axis=1)
-        df['Avg_RR2'],  df['Range_RR2'], df['Nval_RR2']=\
-          zip(*df['RR21'].apply(clean.getListReductions))
-        df.drop('Nval_RR2', axis=1, inplace=True)# Already in Nval
-        ## Set negative RR2 (could not be computed) to 0.0 i.e. no rain
-        ## (any element in the list with error code (<=-99000) will make the average negative)
-        df.loc[df.Avg_RR2 < 1, 'Avg_RR2'] = 0.0
+        if var2prep == 'all' or any("RR2" in s for s in var2prep):
+            if verbose:
+                print 'Clean RR2'
+            df['RR21'] = df[['RadarCounts','RR2']].apply(clean.getIthRadar, axis=1)
+            df['Avg_RR2'],  df['Range_RR2'], df['Nval_RR2']=\
+              zip(*df['RR21'].apply(clean.getListReductions))
+            df.drop('Nval_RR2', axis=1, inplace=True)# Already in Nval
+            ## Set negative RR2 (could not be computed) to 0.0 i.e. no rain
+            ## (elements in the list with error code (<=-99000) will make the average negative)
+            df.loc[df.Avg_RR2 < 1, 'Avg_RR2'] = 0.0
 
         ##RR3
-        df['RR31'] = df[['RadarCounts','RR3']].apply(clean.getIthRadar,
-                                                                                 axis=1)
-        df['Avg_RR3'],  df['Range_RR3'], df['Nval_RR3']=\
-          zip(*df['RR31'].apply(clean.getListReductions))
-        df.drop('Nval_RR3', axis=1, inplace=True)# Already in Nval
-        ## Set negative RR3 (could not be computed) to 0.0 i.e. no rain
-        ## (any element in the list with error code (<=-99000) will make the average negative)
-        df.loc[df.Avg_RR3 < 1, 'Avg_RR3'] = 0.0
+        if var2prep == 'all' or any("RR3" in s for s in var2prep):
+            if verbose:
+                print 'Clean RR3'
+            df['RR31'] = df[['RadarCounts','RR3']].apply(clean.getIthRadar, axis=1)
+            df['Avg_RR3'],  df['Range_RR3'], df['Nval_RR3']=\
+              zip(*df['RR31'].apply(clean.getListReductions))
+            df.drop('Nval_RR3', axis=1, inplace=True)# Already in Nval
+            ## Set negative RR3 (could not be computed) to 0.0 i.e. no rain
+            ## (elements in the list with error code (<=-99000) will make the average negative)
+            df.loc[df.Avg_RR3 < 1, 'Avg_RR3'] = 0.0
 
 
     def show_feature(self, feature):
@@ -158,29 +186,31 @@ class RandomForestModel(object):
         Fits the data and show the score
         '''
         assert(col2fit[0] == 'Expected')
-        print 'Using the following columns:'
+        print '\nScoring with maxdepth={}, nestimators={},\n using the following columns:'.format(maxdepth, nestimators)
         print col2fit[1:]
 
-        ## Get the data ready to fit
-        self.prepare_data(self.df_train)
+        print '\nPreparing/cleaning data...'
+        self.prepare_data(self.df_train, True, col2fit[1:])
         values2fit = self.df_train[col2fit].values
         forest = RandomForestClassifier(n_estimators=nestimators, max_depth=maxdepth)
         nrows = self.df_train.shape[0]
         nfit = int(0.7*nrows)
 
         ## Fit on 70% of the score
+        print '\nFitting...'
         forest.fit(values2fit[:nfit,1:], values2fit[:nfit,0])
 
         ## Predict on the rest of the sample
+        print '\nPredicting...'
         output = forest.predict(values2fit[nfit:,1:])
 
-        print '\nFeatures importances'
+        print '\nDone!\n\nFeatures importances'
         ord_idx = N.argsort(forest.feature_importances_)#Feature index ordered by importance 
         for ifeaturindex in ord_idx[::-1]:
             print '{0} \t: {1} '.format(col2fit[1:][ifeaturindex], round(forest.feature_importances_[ifeaturindex], 2))
 
         ## Get and print the score
-        print 'Scoring...'
+        print '\nScoring...'
         score = kaggle_metric(N.round(output), values2fit[nfit:,0])
         score_pois = kaggle_metric(N.round(output), values2fit[nfit:,0], 'poisson')
         print '\n\nScore(heaviside)={}'.format(score)
@@ -225,7 +255,7 @@ class RandomForestModel(object):
         fig.show()
         raw_input('press enter when finished...')
 
-    def learning_curves(self, col2fit, score='accuracy', nestimators=40, maxdepth=8):
+    def learning_curves(self, col2fit, score='accuracy', nestimators=40, maxdepth=8, verbose=1):
         """
         WARNING: turns the Expected into integer
         Creates a plot score vs # of training examples
@@ -245,10 +275,16 @@ class RandomForestModel(object):
         train_data = self.df_train[col2fit].values
         X = train_data[0:,1:]
         y = train_data[0:,0]
-        train_sizes = [x / 10.0 for x in range(1, 11)]##Can be other formats
+        #train_sizes = [x / 10.0 for x in range(1, 11)]##Can be other formats
+        nsizes = 10
+        train_sizes = [x / float(nsizes) for x in range(1, nsizes + 1)]##Can be other formats
 
-        print 'learning...'
-        train_sizes, train_scores, test_scores = learning_curve(RandomForestClassifier(n_estimators=nestimators, max_depth=maxdepth), X, y, cv=10, n_jobs=1, train_sizes=train_sizes, scoring=score)
+        ## Number of cpu to use
+        ## Making sure there is one free unless there is only one
+        njobs = max(1, multiprocessing.cpu_count() -1)
+        
+        print '\n\nlearning with njobs = {}\n...\n'.format(njobs)
+        train_sizes, train_scores, test_scores = learning_curve(RandomForestClassifier(n_estimators=nestimators, max_depth=maxdepth, verbose=verbose), X, y, cv=10, n_jobs=njobs, train_sizes=train_sizes, scoring=score)
 
         ## Plotting
         fig = plt.figure()
@@ -272,9 +308,10 @@ class RandomForestModel(object):
         plt.legend(loc="best")
         print 'Done'
         fig.show()
+        plt.savefig('learningcurve.png')
         raw_input('press enter when finished...')
 
-    def submit(self, col2fit, maxdepth=8, nestimators = 30):
+    def submit(self, col2fit, maxdepth=8, nestimators = 30, test = False, verbose=1):
         '''
         Create the file to submit
         '''
@@ -283,22 +320,53 @@ class RandomForestModel(object):
         print col2fit
         ## Prepare train and test data
         print 'Cleaning train data...'
-        self.prepare_data(self.df_train)
-        print 'Getting and cleaning test data...'
-        #df_test = pd.read_csv('Data/test_2014.csv', nrows=2000)##For testing
-        df_test = pd.read_csv('Data/test_2014.csv')
-        self.prepare_data(df_test)
+        self.prepare_data(self.df_train, True)
+        
+        njobs = 2
+        ## Number of cpu to use
+        ## Making sure there is one free unless there is only one
+        #njobs = max(1, multiprocessing.cpu_count() -1)
 
-        print 'Training with maxdepth={} and n_estimators={}...'.format(maxdepth, nestimators)
+        print 'Training with maxdepth={}, n_estimators={}, n_jobs={}...'.format(maxdepth, nestimators, njobs)
         values2fit = self.df_train[col2fit].values
-        forest = RandomForestClassifier(n_estimators=nestimators, max_depth=maxdepth, n_jobs=-1)
+
+        ## Clean memory
+        self.df_train = None
+        gc.collect()
+
+        print '\n\nFitting...'
+        forest = RandomForestClassifier(n_estimators=nestimators, max_depth=maxdepth, n_jobs=njobs, verbose=verbose)
         forest.fit(values2fit[:,1:], values2fit[:,0])
 
-        print 'Predicting...'
-        values4predict = df_test[col2fit[1:]].values
-        prediction_output = forest.predict(values4predict)
+        ##Save the output
+        print '\nSaving...'
+        savename = 'saved_fit/svforest.pkl'
+        from sklearn.externals import joblib
+        joblib.dump(forest, savename)
+        print 'Saving in fit in {}'.format(savename)
 
-        print 'Create submission data...'
+        if test:
+            ntest = 2000
+            print '\nGetting and cleaning test data... only {} rows for testing'.format(ntest)
+            df_test = pd.read_csv('Data/test_2014.csv', nrows=ntest)
+        else:
+            print '\nGetting and cleaning all test data...'
+            df_test = pd.read_csv('Data/test_2014.csv')
+        list_id = df_test['Id'].values
+        self.prepare_data(df_test, True)
+
+        values4predict = df_test[col2fit[1:]].values
+
+        ## Clean memory
+        print '\nRemoving df_test from memory'
+        del df_test
+        gc.collect()
+
+        print '\nPredicting...'
+        prediction_output = forest.predict(values4predict)
+        
+
+        print '\nCreate submission data...'
         ## Heaviside
         #submission_data = N.array(map(heaviside, N.round(prediction_output)))
         submission_data = N.array(map(poisson_cumul, N.round(prediction_output)))
@@ -318,22 +386,25 @@ class RandomForestModel(object):
             raw_input('press enter...')
         '''
 
-        list_id = df_test['Id'].values
         solution.generate_submission_file(list_id,submission_data)
-        #print '\n\n\n Done!'
+        print '\n\n\n Done!'
 
 
                 
 if __name__=='__main__':
-    rfmodel = RandomForestModel('Data/train_2013.csv', 200000)
+    rfmodel = RandomForestModel('Data/train_2013.csv', 20000)
     #rfmodel = RandomForestModel('Data/train_2013.csv', 'all')
     #rfmodel.show_feature('Avg_RR3')
+    #coltofit = ['Expected', 'Avg_Reflectivity', 'Range_Reflectivity', 'Nval',
+    #            'Avg_DistanceToRadar', 'Avg_RadarQualityIndex', 'Range_RadarQualityIndex',
+    #            'Avg_RR1', 'Range_RR1','Avg_RR2', 'Range_RR2','Avg_RR3', 'Range_RR3']
     coltofit = ['Expected', 'Avg_Reflectivity', 'Range_Reflectivity', 'Nval',
-                'Avg_DistanceToRadar', 'Range_DistanceToRadar',
-                'Avg_RadarQualityIndex', 'Range_RadarQualityIndex', 'Avg_RR1', 'Range_RR1','Avg_RR2', 'Range_RR2','Avg_RR3', 'Range_RR3']
-    rfmodel.fitNscore(coltofit)
+                'Avg_RadarQualityIndex', 'Avg_RR1', 'Range_RR1', 'Range_RR2', 'Range_RR3']
+    #coltofit = ['Expected', 'Avg_Reflectivity', 'Range_Reflectivity', 'Nval']
+    rfmodel.fitNscore(coltofit, 9, 40)
     #rfmodel.validation_curves(coltofit)
     #rfmodel.learning_curves(coltofit)
 
     ##Submission
+    #rfmodel.submit(coltofit, 8, 30, True)
     #rfmodel.submit(coltofit)
