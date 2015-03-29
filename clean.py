3e2cb2eca2a2d945b809d09e5577fbb3c2f6cb49
Bruno Rousseau <rousseau.bruno@gmail.com>
2015-03-29 10:51:55 -0400

    New function to extract single radar entries
    The function takes in the dataframe as read from the training .csv and
    produce a new dataframe where all rows now represent single radar
    observation. The "Id" label is preserved, all rows having the same
    Id coming from the same initial input dataframe row. A new unique
    identifier, uniqueId, is added as well.

--- clean.py
+++ clean.py
@@ -1,4 +1,6 @@
 import numpy as N
+import pandas as pd
+
 
 def getRadarLength(TimeToEnd):
     """
@@ -54,7 +56,7 @@ def separate_listInColumn(x):
     return tuple(by_rads)
 
 def getIthRadar(x, iradar =1):
-    '''
+    """
     Returns a list of measurements for the ith radar
     Returns None if there are no ith radar
     Input:
@@ -63,7 +65,7 @@ def getIthRadar(x, iradar =1):
                 (as produced by getRadarLength)
                 - Second columns must contains the values to separate
         iradar : the ith radar to return the data from (default=1st)
-    '''
+    """
     if len(x.iloc[0])<iradar:
         return None## Or should it be NA?
     ## The longer but clearer way
@@ -76,24 +78,131 @@ def getIthRadar(x, iradar =1):
     return tuple(listrads[rad_start_index:rad_stop_index])
 
 def getListReductions(x):
-    '''
+    """
     Returns mean, range (max - min) and # of values of the given list
     Input:
         x : should be a list or tuple
         (or something that can be turned into a numpy array)
-    '''
+    """
     xarray = N.array(x)
     return xarray.mean(), xarray.ptp(axis=0), len(xarray)
 
 def getStringReductions(x):
-    '''
+    """
     Returns mean, range (max - min) and # of values of the given string
     Input:
         x : should be a space-separated value string
-    '''
+    """
     xarray = N.array(map(float, x.split()))
     return xarray.mean(), xarray.ptp(axis=0), len(xarray)
 
+
+def get_dataframe_with_split_multiple_radars(input_df):
+    """
+    Separates the rows containing multiple radar time series into mutliple rows, introducing a new
+    column identifying the group the new rows came from.
+    Input:
+        raw pandas dataframe, as read directly from the training dataset.
+    Output:
+        new pandas dataframe, with each row corresponding to an individual radar.
+    """
+
+    # Get a list of all the columns that will have to be split by radar.
+    columns_to_split = list(input_df.columns)
+
+    columns_to_split.remove('Id')       # not a time series!
+    columns_to_split.remove('Expected') # not a time series!
+
+    # Create an array of all the column names, in the same order as in the 
+    # original data.
+    right_column_order = ['unique_Id']
+    for col in input_df.columns:
+        right_column_order.append(col)
+             
+    # Append a new column that will represent how many radars were in the set from
+    # which a given row came from.
+    right_column_order.append('number_of_radars')
+
+    # Loop over all rows of the input dataframe, splitting mutliple radar rows
+    # as we go along.
+
+    # note about the algorithm below:
+    #   It seems pretty ugly to loop on an index (not very pythonic). 
+    #   However, since I don't know ahead of time how many rows I'll have,
+    #   a stackoverflow comment suggests that creating a list of dictionaries is faster.
+    #   See: http://stackoverflow.com/questions/10715965/add-one-row-in-a-pandas-dataframe
+    #
+
+    # list of dictionaries, which will become our new dataframe
+    list_new_dataframe_dict = []
+
+    id_counter = -1  # unique identifier for our split radar entries
+    for index in range(len(input_df)):    
+
+        # create a copy of the row, so we can manipulate
+        # it without polluting the initial dataframe. 
+        row = input_df.loc[index].copy()
+
+        ID       = row['Id']
+        expected = row['Expected']
+
+        # don't want to pollute input dataframe! We're 
+        # hacking the copy here.
+        row['RadarCounts'] = getRadarLength(row['TimeToEnd'])
+        number_of_radars   = len(row['RadarCounts'])
+
+        # list of dictionaries spawned by this row
+        list_newrows_dict = []
+
+        # initialize all relevant dictionaries with 
+        # "family" data, ie stuff that is the same for all radars in row. 
+        for i in row['RadarCounts']:
+            id_counter += 1
+            list_newrows_dict.append({        'unique_Id':id_counter,
+                                                     'Id':ID, 
+                                       'number_of_radars':number_of_radars, 
+                                               'Expected':expected          })
+
+        # populate the new dictionaries just created above with every column data
+        for col in columns_to_split:
+
+            # get subrow so we can apply splitting methods            
+            subrow = row[['RadarCounts',col]]
+
+            # fill the dictionaries with the split data
+            for array, dict in zip(separate_listInColumn(subrow),list_newrows_dict):
+                dict[col] = N.array(array)
+
+
+        # extend the main list of dictionaries with the entries from this row        
+        list_new_dataframe_dict.extend( list_newrows_dict )
+
+    # create the new dataframe from the list of dictionaries
+    output_df = pd.DataFrame(list_new_dataframe_dict)[right_column_order].set_index('unique_Id')
+
+    return output_df 
+
+
+    
+if __name__ == "__main__":
+    #print getRadarLength([5,4,3,2,1])
+    #print getRadarLength([5,4,3,7,1])
+
+    ##The following lines show how to create a new column with the length of each radar
+    import pandas as pd
+    data = {"a": ["5 4 3 2 1", "5 4 3 7 1", "6 7 7", "3 5 6 1", "1 2 3 4 5 6 7 8"], "b" :  [(3,2), (2,3), (1,2), (2,2), (2,3,2)]}
+    df = pd.DataFrame(data)
+    print df
+    #df['z'] = df['a'].apply(getRadarLength)
+    #print df[[1,0]].apply(separate_listInColumn, axis=1)
+    #df['z'] = df[[1,0]].apply(separate_listInColumn, axis=1)
+    #print df[['b','a']].apply(separate_listInColumn, axis=1)
+    #df['r1'], df['r2'] = zip(*df[['b','a']].apply(separate_listInColumn, axis=1))
+    #print '\n\n\n'
+    #print df
+    df['a1'] =  df[['b','a']].apply(getIthRadar, axis=1)
+    print list(df['a1'])
+    #print zip(*df['a'].apply(getListReductions))
     
 if __name__ == "__main__":
     #print getRadarLength([5,4,3,2,1])
